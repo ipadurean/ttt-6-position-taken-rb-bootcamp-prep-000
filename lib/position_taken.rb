211e9ad76e1cdf34640fcba5ee07b423ac1f3f5e
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-04 00:52:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,11 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] == " "
-    "false"
+    false
   elsif board[index] == ""
-    "false"
+    false
   elsif board[index] == nil
-    "false"
+    false
   else
     true
   end
